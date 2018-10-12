d4484eaf3148bfc8c92bd5ed3fc6d68748e92974
Neha Pathak <neha.pathak@hotmail.com>
2018-10-11 20:03:44 -0400

    testing PHP component

---
+++ UnbrandedForm/contactformsettings.php
@@ -0,0 +1,20 @@
+<?php
+
+$email_to = "neha.pathak@hotmail.com"; 
+$email_subject = "Portfolio: Inquiry"; 
+$thankyou = "thankyou.htm"; // thank you page
+
+$antispam_answer = "25";
+
+
+// If you have problems receiving emails, 
+// change the option below:
+// OPTIONS: "default", "sendmail" or "smtp"
+$emailing_option = "default";
+
+// if you are using "smtp" add the details here
+$smtp_host = "smtp.mandrillapp.com";
+$smtp_port = "587";
+$smtp_user = "";
+$smtp_pass = "";
+?>
