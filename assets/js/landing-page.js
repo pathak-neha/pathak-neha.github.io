5b944d5d091d1d9e867e7ceb6974d4983ef099af
Neha Pathak <neha.pathak@hotmail.com>
2018-11-04 22:33:07 -0500

    updated landing page

---
+++ assets/js/landing-page.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    var particles = Particles.init({
+        selector: '.background',
+      color: ['#DA0463', '#404B69', '#DBEDF3'],
+      connectParticles: true,
+      maxParticles: 170,
+      sizeVariations: 4,
+      speed: 1
+    });
+};
